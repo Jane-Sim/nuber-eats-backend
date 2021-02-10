5ee358f3702bc51ca44868093e952c0f56a1a731
seyoung.sim <ssiox3@gmail.com>
2021-02-10 15:09:20 +0900

    class-validator, class-transformer를 이용한 데이터 유효성검사

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
+/**
+ * NestJs의 application을 실행시키는 파일.
+ * main.ts는 이름이 변경이 없어야한다.
+ * NestFactory에서 AppModule을 가져와 App을 생성하고,
+ * 생성한 app에 ValidationPipe 파이프를 설치하여, 데이터 유효성 검사를 한다.
+ */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -2,17 +2,27 @@
  * 주고 받는 데이터의 타입을 지정하는 dto.
  * restaurant의 데이터를 생성할 때 사용한다.
  * ArgsType 데코레이터를 통해, 클라이언트가 전달하는 인자 값으로 데이터를 생성.
+ * class-validator로 데이터의 유효성 검사를 한다.
  */
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field((type) => Boolean)
+  @IsBoolean()
   isVegan: boolean;
+
   @Field((type) => String)
+  @IsString()
   adddress: string;
+
   @Field((type) => String)
+  @IsString()
   ownersName: string;
 }
