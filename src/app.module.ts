356c5ff2bd4359ac73023f62b55d8d07d440cf1d
Sim Seyoung <ssiox3@gmail.com>
2021-02-19 03:32:02 +0900

    데이터베이스 보안을 위한 config의 ConfigService 설정.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,21 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   imports: [
     // NestJS의 config 설정을 통해 dotenv로 환경변수 파일인(.env)를 사용한다.
     // isGlobal은 어디서나 config 모듈에 접근 가능옵션, envFilePath는 .env파일의 경로.
+    // ignoreEnvFile은 서버에 deploy시, .env파일의 사용여부를 설정. prod환경에서만 .env를 사용하지 않는다.
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     // TypeORM과 postgres의 드라이버 설정을 한다.
+    // cross-env로 가져온 .env의 변수값을 가져와 드라이버 설정을 한다.
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'hongsimi',
-      password: 'test',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
