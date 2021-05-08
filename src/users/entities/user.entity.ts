685a082c846e16c428b9d443f5382791fc4ef216
janeSim <ssiox3@gmail.com>
2021-05-09 00:10:34 +0900

    User의 비밀번호 값을 해싱하도록 typeorm의 BeforeInsert 데코레이터 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,15 @@
 // user의 entity. core entity를 상속받아서 사용한다.
-
+// 사용자의 비밀번호는 bcrypt 라이브러리를 이용해서 hash화 시킨다.
+import { InternalServerErrorException } from '@nestjs/common';
 import {
   Field,
   InputType,
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
+import * as bcrypt from 'bcrypt';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 
 // 사용자의 권한을 사용하는 enum
 enum UserRole {
@@ -35,4 +37,18 @@ export class User extends CoreEntity {
   @Field((type) => UserRole)
   @Column({ type: 'enum', enum: UserRole })
   role: UserRole;
+
+  // typeorm에서 제공하는 Entity Listeners and Subscribers중 하나인
+  // @BeforeInsert() 데코레이터를 이용하여, typeorm이 db에 데이터를 저장하기 전에,
+  // Repository.save() 함수 실행 전에, password 속성 값을 해싱한다.
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      // bcrypt.hash 함수에 해싱시킬 데이터와, round 값을 넘긴다. (round default = 10)
+      // salt round는 해싱 계산 횟수를 뜻한다. 값이 높을 수록 계산 수 up. ex) 2^10 = 1000회
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
 }
