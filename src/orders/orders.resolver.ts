1dc03c789a9e70600e794a51f13ec093ac9d381c
seyoung.sim <ssiox3@gmail.com>
2021-05-28 11:34:33 +0900

    subScription의 resolve 예제

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -77,14 +77,20 @@ export class OrderResolver {
   }
 
   // pubsub의 asyncIterator로 실시간 구독의 트리거를 생성한다.
-  // asyncIterator(트리거 이름)
-  // 특정 조건에 해당되는 실시간 구독을 받고 싶을 때, filter 기능을 사용한다.
+  // asyncIterator(트리거 이름) DB에 저장하지 않는 데이터를 pubSub으로 유저에게 보낼 때 유용하다.
+  // ------------------------------------
+  // 특정 조건에 해당되는 실시간 구독을 받고 싶을 때, filter 기능을 사용한다. true, false 값을 반환하자.
   // filter(publish에서 보낸 payload, readyPotato에서 설정한 Argument potatoId인 variables, graphqlContext) 3개의 파라미터가 설정되어있다.
   // potatoReady의 potatoI와 readyPotato potatoId가 일치하면,알람을 받게된다.
+  // ------------------------------------
+  // 사용자가 받는 실시간 구독 내용 형태를 바꾸고 싶을 때 resolve 기능을 사용한다.
+  // filter와 비슷한 인자 값을 받으나, info 가 마지막 인자로 추가된다. (payload, args, context, info)
   @Subscription((returns) => String, {
     filter: ({ readyPotato }, { potatoId }) => {
       return readyPotato === potatoId;
     },
+    resolve: ({ readyPotato }) =>
+      `Your potato with the id ${readyPotato} is ready!`,
   })
   @Roles('Any')
   readyPotato(@Args('potatoId') potatoId: number) {
